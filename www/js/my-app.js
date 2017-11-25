deee819f0115408f0e6ec523130a701ede392a7e
stefoBark <steven.j.barker.jr@gmail.com>
2017-11-24 17:07:37 -0800

    vertical radio

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,111 +1,77 @@
+/**
+ * Type some JavaScript here and click either
+ * fix or diff.
+ */
 // Initialize app
 var myApp = new Framework7();
-
-
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
-
 // Add view
 var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
-    dynamicNavbar: true
+  // Because we want to use dynamic navbar, we need to enable it for this view:
+  dynamicNavbar: true
 });
-
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
-    
-	
 });
 
-	function processBasic(reading,response){
-		var fullName = $$('#'+reading+'fullName').val();
-		var birthDate = $$('#'+reading+'birthDate').val();
-		var goodDate =  birthDate.split("-");
-		$$.ajax({
-		  url: 'https://numerology.net/phone.php?code='+reading+'|'+fullName+'|'+goodDate[1]+'/'+goodDate[2]+'/'+goodDate[0]+'|',
-		  success: function(e){
-			  $$('#'+response).html(e);
-		  },
-		  error: function(e){
-			  alert('error');
-		  }
-		});
-	}
-
-	function relationship(typeOfReading){
-		var fullNameOne = $$('#fFullName').val();
-		var birthDateOne = $$('#fBirthDate').val();
-		var fullNameTwo = $$('#sFullName').val();
-		var birthDateTwo = $$('#sBirthDate').val();
-		var goodDateOne =  birthDateOne.split("-");
-		var goodDateTwo =  birthDateTwo.split("-");
-		var url = 'https://numerology.net/relphone.php?code='+typeOfReading+'|'+fullNameOne+'|'+goodDateOne[1]+'/'+goodDateOne[2]+'/'+goodDateOne[0]+'|'+fullNameTwo+'|'+goodDateTwo[1]+'/'+goodDateTwo[2]+'/'+goodDateTwo[0]+'|';
-		
-		$$.ajax({
-		  url: url,
-		  success: function(e){
-			  $$('#relationshipResponse').html(e);
-		  },
-		  error: function(e){
-			  alert('error');
-			  alert(url);
-		  }
-		});
-	}
-
-	$$('#submitDestiny').on('click',function(e){
-		processBasic('D','dResponse');
-	});
-	
-	$$('#submitHearts').on('click',function(e){
-		processBasic('H','hResponse');
-	});
-	
-	$$('#submitPersonality').on('click',function(e){
-		processBasic('P','pResponse');
-	});
-	
-	$$('#submitBirthForce').on('click',function(e){
-		processBasic('B','bResponse');
-	});
-	
-	$$('#submitPersonalYear').on('click',function(e){
-		processBasic('Y','pYResponse');
-	});
-	$$('#submitRelationship').on('click',function(e){
-		var radioValue = $$("input[name='type']:checked").val();
-		relationship(radioValue);
-	});
-
-function getGoodDate(date) {
-	var month = date.getMonth() + 1;
-	var day = date.getDate();
-	var year = date.getFullYear();
-	return month + "/" + day + "/" + year;
+function processBasic(reading, response) {
+  var fullName = $$('#' + reading + 'fullName').val();
+  var birthDate = $$('#' + reading + 'birthDate').val();
+  var goodDate = birthDate.split("-");
+  $$.ajax({
+    url: 'https://numerology.net/phone.php?code=' + reading + '|' + fullName + '|' + goodDate[1] + '/' + goodDate[2] + '/' + goodDate[0] + '|',
+    success: function(e) {
+      $$("#" + response).html(e);
+    },
+    error: function(e) {
+      alert(e);
+    }
+  });
 }
 
-
-// Now we need to run the code that will be executed only for About page.
-
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
-
-})
-
-// Option 2. Using one 'pageInit' event handler for all pages:
-$$(document).on('pageInit', function (e) {
-    // Get page data from event data
-    var page = e.detail.page;
-
-    if (page.name === 'about') {
-        // Following code will be executed for page with data-page attribute equal to "about"
-        myApp.alert('Here comes About page');
+function relationship(typeOfReading) {
+  var fullNameOne = $$("#fFullName").val();
+  var birthDateOne = $$("#fBirthDate").val();
+  var fullNameTwo = $$("#sFullName").val();
+  var birthDateTwo = $$("#sBirthDate").val();
+  var goodDateOne = birthDateOne.split("-");
+  var goodDateTwo = birthDateTwo.split("-");
+  var url = "https://numerology.net/relphone.php?code=" + typeOfReading + "|" + fullNameOne + "|" + goodDateOne[1] + "/" + goodDateOne[2] + "/" + goodDateOne[0] + "|" + fullNameTwo + "|" + goodDateTwo[1] + "/" + goodDateTwo[2] + "/" + goodDateTwo[0] + "|";
+  $$.ajax({
+    url: url,
+    success: function(e) {
+      $$("#relationshipResponse").html(e);
+    },
+    error: function(e) {
+      alert('error');
+      alert(url);
     }
-})
+  });
+}
+$$('#submitDestiny').on("click", function(e) {
+  processBasic("D", "dResponse");
+});
+$$('#submitHearts').on('click', function(e) {
+  processBasic("H", "hResponse");
+});
+$$("#submitPersonality").on("click", function(e) {
+  processBasic("P", "pResponse");
+});
+$$('#submitBirthForce').on("click", function(e) {
+  processBasic("B", "bResponse");
+});
+$$('#submitPersonalYear').on("click", function(e) {
+  processBasic("Y", "pYResponse");
+});
+$$("#submitRelationship").on("click", function(e) {
+  var radioValue = $$("input[name='type']:checked").val();
+  relationship(radioValue);
+});
 
-// Option 2. Using live 'pageInit' event handlers for each page
-$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
-    // Following code will be executed for page with data-page attribute equal to "about"
-    myApp.alert('Here comes About page');
-})
+function getGoodDate(date) {
+  var month = date.getMonth() + 1;
+  var day = date.getDate();
+  var year = date.getFullYear();
+  return month + "/" + day + "/" + year;
+}
